922ff69248b4704e8b3e99f6fb3b9101370b7f54
Junobee25 <wnsdhqo@naver.com>
2023-09-13 15:32:13 +0900

    Feat : Todo리스트 시작

--- src/pages/TodoPage/index.jsx
+++ src/pages/TodoPage/index.jsx
@@ -11,68 +11,21 @@ function TodoPage() {
             navigate('/signin');
         }
     }, [jwtToken,navigate]); // 빈 배열을 전달하여 컴포넌트가 마운트된 후에 한 번만 실행
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-
-    // 에러메세지 -> 처음 상태 => 빈값 
-    // 이메일, 비밀번호 마다 => 에러메세지 렌더링 필요할 듯
-    // const [emailerror, setEmailError] = useState("");
-    // const [passworderror, setPasswordError] = useState("");
-
-
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-        
-    
-    };
-
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-
-    };
-
-    // 유효성 검사 변수
-    let checkValid = true
-
-
-    // 유효성 확인 => disable => false
-    if (email.includes("@") && password.length>=8) {
-        checkValid = false
-    }
-
-    const handleSignInClick = async () => {
-        console.log(email,password)
-        // 회원가입 데이터
-        try {
-        const signupData = {
-            email: email, // 이메일 값 입력
-            password: password,      // 비밀번호 값 입력
-        };
-        console.log(signupData)
-
-        const response = await axios.post('https://www.pre-onboarding-selection-task.shop/auth/signin/',
-        signupData, { headers: {'Content-Type':'application/json'}});
-
-        if (response.status){
-            console.log("회원가입 성공")
-            navigate('/')
-        } else {
-            console.log("회원가입 실패")
-        }
-        } catch (error) {
-        console.log(error)
-        }
-    }
-
 
     return (
     <div>
-        EMAIL : <input data-testid="email-input" value={email} onChange={handleEmailChange}/>
-        PASSWORD : <input data-testid="password-input" type="password" value={password} onChange={handlePasswordChange}/>
-        <button data-testid="signin-button" onClick={handleSignInClick} disabled={checkValid}>로그인</button>
-
-        
+        <li>
+            <label>
+                <input type="checkbox" />
+                <span>TODO 1</span>
+            </label>
+        </li>
+        <li>
+            <label>
+                <input type="checkbox" />
+                <span>TODO 2</span>
+            </label>
+        </li>
     </div>
     )
 }
